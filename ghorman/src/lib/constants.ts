1d286b7139a4127dd4117073faafd986190f3b3d
James Yap <contact@jamesyap.org>
2025-07-28 22:21:29 -0400

    refactor: major migration (out of OOP)

---
+++ ghorman/src/lib/constants.ts
@@ -0,0 +1,1 @@
+export const FLOAT32_SIZE = 4; // bytes
---
+++ ghorman/src/main.ts
@@ -0,0 +1,57 @@
+import { FLOAT32_SIZE } from './lib/constants';
+import { compileShader, createProgram, resize } from './lib/utils';
+import './style.css'
+
+
+
+
+async function main() {
+  // Get canvas and webgl context
+  const canvas = document.getElementById('webgl-canvas')! as HTMLCanvasElement;
+  const gl = canvas.getContext('webgl2')!;
+
+  // Resize
+  resize(gl, canvas);
+  window.addEventListener('resize', () => {
+    resize(gl, canvas);
+  });
+
+  // Shaders
+  const vertexShader = await compileShader(gl, 'shaders/points.vert', gl.VERTEX_SHADER);
+  const fragmentShader = await compileShader(gl, 'shaders/points.frag', gl.FRAGMENT_SHADER);
+
+  // Program
+  const program = createProgram(gl, vertexShader, fragmentShader)!;
+
+  // Buffer
+  const vertices = new Float32Array([
+    -0.5, -0.5, 0,
+    0.5, -0.5, 0,
+    0, 0.5, 0
+  ]);
+  const vbo = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+  gl.vertexAttribPointer(0, 3, gl.FLOAT, false, FLOAT32_SIZE * 3, 0);
+  gl.enableVertexAttribArray(0);
+  gl.useProgram(program);
+
+
+  function animate() {
+    // Clear background
+    gl.clearColor(0, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.POINTS, 0, 3);
+    requestAnimationFrame(animate);
+  }
+  animate();
+
+  // Cleanup
+  window.onbeforeunload = () => {
+    gl.deleteProgram(program);
+    gl.deleteBuffer(vbo);
+  };
+}
+
+main();
