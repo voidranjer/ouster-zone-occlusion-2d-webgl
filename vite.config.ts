8a9494823bd4a85cdaea145ffa043955942d13e9
James Yap <james.yap@ouster.io>
2025-08-15 10:33:30 -0400

    fix: BASE_URL for GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  // Set the base URL from an environment variable
+  base: process.env.VITE_BASE_URL || '/',
+
   plugins: [
     tailwindcss(),
   ],
