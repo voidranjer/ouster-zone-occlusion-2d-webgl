773d6848652dc05540aa4041f44c405948144ff2
James Yap <james.yap@ouster.io>
2025-08-15 13:23:46 -0400

    refactor: installed react

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   // Set the base URL from an environment variable
   base: process.env.VITE_BASE_URL || '/',
 
   plugins: [
     tailwindcss(),
+    react()
   ],
 })
